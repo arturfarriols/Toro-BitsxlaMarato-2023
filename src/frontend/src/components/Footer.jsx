e5fbea46b95068cafb7994b3272105411f0e40b7
ehlui <86680816+ehlui@users.noreply.github.com>
2023-12-17 01:34:37 +0100

    Adding Frontend - v1 - Simple UI with BE Connection

---
+++ src/frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-white  shadow">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 &nbsp; 
+                                    <a href="https://github.com/arturfarriols/Toro-BitsxlaMarato-2023" className="hover:underline">Toro-BitsxlaMarato</a>.
+                </span>
+                <ul className="">
+                  
+                </ul>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Footer;
